04577119b5d9419bc2294f745487dca04922a41a
MarckDmarck <155605126+MarckDmarck@users.noreply.github.com>
2024-03-03 01:12:31 +0000

    Se factorizo el código del archivo app.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,17 @@
 window.onload = () => {
-  let personaje = Array();
-  personaje[0] = "El gato";
-  personaje[1] = "El perro";
-  personaje[2] = "El carnicero";
+  let personaje = ["El gato", "El perro", "El carnicero"];
+  let accion = ["enterro mi tarea", "rompio mi cuaderno", "orino mi lapiz"];
+  let tiempo = ["luego de la clase", "justo ahora", "hace un rato"];
 
-  let accion = Array();
-  accion[0] = "enterro mi tarea";
-  accion[1] = "rompio mi cuaderno";
-  accion[2] = "orino mi lapiz";
-
-  let tiempo = Array();
-  tiempo[0] = "luego de la clase";
-  tiempo[1] = "justo ahora";
-  tiempo[2] = "hace un rato";
-  {
-    document.querySelector("#mensaje1").innerHTML =
-      personaje[Math.floor(Math.random() * personaje.length)];
-    document.querySelector("#mensaje2").innerHTML =
-      accion[Math.floor(Math.random() * accion.length)];
-    document.querySelector("#mensaje3").innerHTML =
-      tiempo[Math.floor(Math.random() * tiempo.length)];
-  }
   var person = (document.querySelector("#mensaje1").innerHTML =
     personaje[Math.floor(Math.random() * personaje.length)]);
+
   var accion1 = (document.querySelector("#mensaje2").innerHTML =
     accion[Math.floor(Math.random() * accion.length)]);
+
   var tiempo1 = (document.querySelector("#mensaje3").innerHTML =
     tiempo[Math.floor(Math.random() * tiempo.length)]);
+
   console.log(person);
   console.log(accion1);
   console.log(tiempo1);
